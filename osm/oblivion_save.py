8ed62e946b536ee746bdb5d74c1f1a1d0189c0a1
Czyzx <sapphique.6180@gmail.com>
2024-07-27 02:27:40 -0400

    Create OblivionSave class. Create functions in OblivionSaveReader to read FileHeader, SaveHeader, Plugins and Globals.

--- osm/oblivion_save.py
+++ osm/oblivion_save.py
@@ -3,6 +3,24 @@ import struct
 from oblivion_types import *
 
 
+class OblivionSave:
+    def __init__(self, filename):
+        self.reader = OblivionSaveReader(filename)
+
+        self.file_header: FileHeader = self.reader.read_file_header()
+        self.save_header: SaveHeader = self.reader.read_save_header()
+        self.plugins: list[bytes] = self.reader.read_plugins()
+        self.globals: Globals = self.reader.read_globals()
+
+    def __del__(self):
+        del self.reader
+
+    def __str__(self):
+        return f"OblivionSave({self.reader.filename})"
+
+    def __repr__(self):
+        return self.__str__()
+
 class OblivionSaveReader:
     def __init__(self, filename):
         self.filename = filename
@@ -12,7 +30,132 @@ class OblivionSaveReader:
     def __del__(self):
         self.file.close()
 
-    
+    def read_file_header(self):
+        header = self.read_bytes(12)
+        major_version = self.read_u8()
+        minor_version = self.read_u8()
+        exe_time = self.read_bytes(16)
+
+        return FileHeader(header, major_version, minor_version, exe_time)
+
+    def read_save_header(self):
+        header_version = self.read_u32()
+        save_header_size = self.read_u32()
+        save_num = self.read_u32()
+        pc_name = self.read_bzstr()
+        pc_level = self.read_u16()
+        pc_cell_name = self.read_bzstr()
+        game_days = self.read_float()
+        game_ticks = self.read_u32()
+        game_time = self.read_bytes(16)
+        screenshot = self.read_screenshot()
+
+        return SaveHeader(
+            header_version,
+            save_header_size,
+            save_num,
+            pc_name,
+            pc_level,
+            pc_cell_name,
+            game_days,
+            game_ticks,
+            game_time,
+            screenshot
+        )
+
+    def read_plugins(self):
+        plugin_num = self.read_u8()
+        plugin_names = [self.read_bstr() for _ in range(plugin_num)]
+
+        return plugin_names
+
+    def read_globals(self):
+        form_ids_offset = self.read_u32()
+        records_num = self.read_u32()
+        next_object_id = self.read_u32()
+        world_id = self.read_u32()
+        world_x = self.read_u32()
+        world_y = self.read_u32()
+        pc_location = PCLocation(
+            self.read_u32(),
+            self.read_float(),
+            self.read_float(),
+            self.read_float()
+        )
+        globals_num = self.read_u16()
+        global_vars = {
+            self.read_u32(): self.read_float()
+            for _ in range(globals_num)
+        }
+        tes_class_size = self.read_u16()
+        num_death_counts = self.read_u32()
+        death_counts = {
+            self.read_u32(): self.read_u16()
+            for _ in range(num_death_counts)
+        }
+        # Number of second spent in-game with no menus open.
+        gametime_seconds = self.read_float()
+        processes_size = self.read_u16()
+        processes_data = self.read_bytes(processes_size)
+        spec_event_size = self.read_u16()
+        spec_event_data = self.read_bytes(spec_event_size)
+        weather_size = self.read_u16()
+        weather_data = self.read_bytes(weather_size)
+        pc_combat_count = self.read_u32()
+        # Created data, this includes spells, enchantments and potions created in-game.
+        # The format is the same as the mod file record format.
+        created_num = self.read_u32()
+        created_records: list[CreatedRecord] = []
+
+
+        for _ in range(created_num):
+            record_type = self.read_bytes(4)
+            record_size = self.read_u32()
+            flags = self.read_u32()
+            form_id = self.read_u32()
+            vc_info = self.read_u32()
+            data = self.read_bytes(record_size)
+
+            record = CreatedRecord(
+                record_type, record_size, flags, form_id, vc_info, data
+            )
+
+            created_records.append(record)
+
+        saved_pos = self.position
+        #   Quick Keys settings. The size of individual data records can be 1 or 5 bytes, depending
+        # upon the value of "flag" in each record.  If flag is 0 the key is not set, if flag is 1
+        # the key is set and an iref follows.
+        quick_keys_size = self.read_u16()
+        quick_keys_data = []
+
+        while self.position < saved_pos + quick_keys_size:
+            flag = self.read_u8()
+            
+            if flag == 0:
+                quick_keys_data.append(0)
+            else:
+                quick_keys_data.append(1)
+                quick_keys_data.append(self.read_u32())
+
+        # Reticle data, not sure what this is yet.
+        reticle_size = self.read_u16()
+        reticle_data = self.read_bytes(reticle_size)
+        # Interface stuff, not sure what this is yet.
+        interface_size = self.read_u16()
+        interface_data = self.read_bytes(interface_size)
+        # Information about regions.
+        regions_save = self.read_u16()
+        regions_num = self.read_u16()
+        regions_data = [(self.read_u32(), self.read_u32()) for _ in range(regions_num)]
+
+        return Globals(
+            form_ids_offset, records_num, next_object_id, world_id, world_x, world_y, pc_location,
+            globals_num, global_vars, tes_class_size, num_death_counts, death_counts, gametime_seconds,
+            processes_size, processes_data, spec_event_size, spec_event_data, weather_size, weather_data,
+            pc_combat_count, created_num, created_records, quick_keys_size, quick_keys_data, reticle_size,
+            reticle_data, interface_size, interface_data, regions_save, regions_num, regions_data
+        )
 
     def read_formatted(self, format, length, offset):
         if offset:
